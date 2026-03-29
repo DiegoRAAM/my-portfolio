2672b61bb8f2369e24c09bfd5aa1a7336b86afce
Diego <diegodevhub16@gmail.com>
2026-03-28 22:44:45 -0600

    feat: formulario de contacto con validación

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ document.querySelector("#inicio a").addEventListener("click", function() {
 document.querySelector("#menu-btn").addEventListener("click", function () {
     document.querySelector("header nav").classList.toggle("activo");
 });
+
+document.querySelector("#formulario").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let nombre = document.querySelector("#campo-nombre").value;
+    let email = document.querySelector("#campo-email").value;
+    let mensaje = document.querySelector("#campo-mensaje").value;
+
+    if (nombre === "" || email === "" || mensaje === "") {
+        document.querySelector("#respuesta").textContent = "Por favor completa todos los campos.";
+        document.querySelector("#respuesta").style.color = "#e94560";
+    }
+
+    else {
+        document.querySelector("#respuesta").textContent = "Gracias " + nombre + ", te contactaré pronto.";
+        document.querySelector("#respuesta").style.color = "#1a1a2e";
+    }
+})
