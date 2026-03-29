55ea12ab01fef7e5a7e8b900132bb55036bda7f5
Diego <diegodevhub16@gmail.com>
2026-03-28 22:04:22 -0600

    feat: menu hamburguesa responsive

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ document.querySelector("main p").textContent = profesion;
 document.querySelector("#inicio a").addEventListener("click", function() {
     alert("Gracias por contactarme, Diego");
 });
+
+document.querySelector("#menu-btn").addEventListener("click", function () {
+    document.querySelector("header nav").classList.toggle("activo");
+});
